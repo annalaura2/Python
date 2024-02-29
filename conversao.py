d4ec9a5ec0612fd4db0c03c9b376002c9868b686
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-02-29 18:15:06 -0300

    Add files via upload

---
+++ conversao.py
@@ -0,0 +1,29 @@
+#int para float
+preco = 10 
+preco = float(preco)
+print(preco)
+
+#float para int
+numero = 24.56
+numero = int(numero)
+print(numero)
+
+#conversao por divisao
+num = 9
+print(num / 2) #float
+print(num// 2) #int
+
+#numero para string 
+preco2 = 10.50
+idade = 28
+print(str(preco2))
+print(str(idade))
+texto = f"idade {idade} e preco {preco2}"
+print(texto)
+
+#string para numero 
+a = "30.79"
+b = "29"; 
+print(float(a))
+print(int(b))
+
