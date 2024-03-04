693a848e1c36a235aa268c18d7abf0860c074bfe
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-03-04 19:50:00 -0300

    Add files via upload

---
+++ operadores_atribuicao.py
@@ -0,0 +1,21 @@
+# Atribuição simples 
+saldo = 800
+print(saldo)
+
+# Atribuição com adição
+saldo += 200
+print(saldo)
+
+# Atribuição com subtração 
+saldo -= 200
+print(saldo)
+
+# Atribuição com divisão 
+saldo /= 2
+print(saldo)
+saldo //= 2
+print(saldo)
+
+# Atribuição com módulo 
+saldo %= 3
+print(saldo)
