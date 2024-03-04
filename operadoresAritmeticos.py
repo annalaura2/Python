b4ce083a3ac36eac30df49d57a95eda301b971d8
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-03-04 19:49:01 -0300

    Add files via upload

---
+++ operadoresAritmeticos.py
@@ -0,0 +1,20 @@
+# Adição 
+print(1 + 3)
+
+# Subtração 
+print(3 - 1)
+
+# Multiplicação
+print(3*1)
+
+# Divisão 
+print(3/1)
+
+# Divisão inteira 
+print(3//1)
+
+# Módulo 
+print(3%1)
+
+# Exponenciação 
+print(2**3)
