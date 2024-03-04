e6e4b22c8cbd790a4b1437c64ac8c8a0be4a5848
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-03-04 19:50:37 -0300

    Add files via upload

---
+++ operadores_comparacao.py
@@ -0,0 +1,13 @@
+saldo = 2000
+saque = 100
+# Igualdade
+print(saldo == saque)
+
+# Diferença
+print(saldo != saque)
+
+# Maior ou igual
+print(saldo >= saque)
+
+# Menor ou igual
+print(saldo <= saque)
