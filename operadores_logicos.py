131dffda1f66ade6648fd851439f7f71ff0da17a
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-03-04 19:51:20 -0300

    Add files via upload

---
+++ operadores_logicos.py
@@ -0,0 +1,14 @@
+saldo = 200
+cheque_esp = 200
+conta = 0
+
+# Operador E (and)
+if(cheque_esp and saldo == 200): {
+    print("iguais")
+}
+
+# Operador OU (or)
+print(saldo >= cheque_esp or saldo <= conta)
+
+# Operador de negação (not)
+print(not saldo == 200)
