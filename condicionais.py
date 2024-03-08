7f0054942263678704dd59ba2c38dce9c5c56d36
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-03-08 14:06:22 -0300

    Add files via upload

---
+++ condicionais.py
@@ -0,0 +1,10 @@
+saldo = 2000.0
+saque = float(input("Informe o saque: "))
+
+if (saldo>= saque):
+    print("saque realizado")
+else:
+    print("saldo insuficiente")
+
+#if (saldo < saque):
+   # print("saldo insuficiente")
