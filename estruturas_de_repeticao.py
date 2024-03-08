05db09df8cbf665c7a6fd894b618d72e71111297
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-03-08 14:08:04 -0300

    Add files via upload

---
+++ estruturas_de_repeticao.py
@@ -0,0 +1,22 @@
+#*nome = input("Informe o nome: ")
+#meu_nome = "ANNA"
+
+#for letra in nome:
+ #   if letra.upper() in meu_nome:
+  #      print(letra, end="")
+
+#print()
+
+for numero in range(0,11):
+    print(numero, end = "\n")
+
+print("tabuada do 5: ")
+
+for numero in range(0, 51, 5):
+    print(numero, end= "\n")
+
+opcao = 1
+
+while opcao ==1:
+    print("eu sou programadora\n")
+    break
