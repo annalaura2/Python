36c57f064093c0af35f1aaebe8610fbd626c099e
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-03-12 10:51:31 -0300

    Add files via upload

---
+++ remove.py
@@ -0,0 +1,5 @@
+# remove - remove a partir do objeto que foi informado
+
+numeros = [1, 4, 6, 8,2 ]
+numeros.remove(4)
+print(numeros)
---
+++ reverse.py
@@ -0,0 +1,4 @@
+# reverse - espelhamento da lista 
+numeros = [1, 2, 3, 4, 5]
+numeros.reverse()
+print(numeros)
