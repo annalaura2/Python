ef9f99a2c82e0e70aea8f687c1890cc57e6c661f
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-03-11 21:40:53 -0300

    Add files via upload

---
+++ interpolacao_de_variaveis.py
@@ -0,0 +1,10 @@
+nome = "Anna Laura"
+idade = 20
+curso = "Ciência da Computação"
+
+print(f"Olá, meu nome é {nome} e tenho {idade} anos. Sou graduanda no quarto período da faculdade de {curso}")
+
+# formatand strings com f-string
+
+pi = 3.14159
+print(f"Valor de PI: {pi: .2f}")
