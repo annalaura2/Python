4c3c838e9d20d3c828011c1b0f91355e088f2ed0
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-03-15 13:01:06 -0300

    Add files via upload

---
+++ dicionario.py
@@ -0,0 +1,9 @@
+pessoa = {"nome": "Anna", "idade:": 20}
+# outra maneira 
+pessoa2 = dict(nome = "Sarah", idade = 21)
+
+#adicionando uma nova chave
+pessoa["telefone"] = "1234-5678"
+
+print(pessoa)
+print(pessoa2)
